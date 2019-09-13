6ac1defcb3da19b02a89f2e4ca4be021b22f3397
songjun <songjun@broada.com>
2019-09-14 00:26:13 +0800

    update:完成todo 的增删

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,31 +1,71 @@
 import * as React from "react";
+import { connect } from "react-redux";
+import { State } from "../store/reducer";
+import { deleteTodo, toggleTodo, Todo } from "../store/action";
 
-class Content extends React.Component<{}, {}> {
-  constructor(props: any) {
+interface Props {
+  onDeleteTodo?: (id: number) => any;
+  onToggleTodo?: (id: number) => any;
+  todo?: { todoList: [] };
+}
+interface States {}
+
+class Content extends React.Component<Props, States> {
+  constructor(props: Props) {
     super(props);
   }
+
+  handleToggle(id: number) {
+    this.props.onToggleTodo(id);
+  }
+
+  handleDelete(id: number) {
+    this.props.onDeleteTodo(id);
+  }
+
   render() {
+    const { todoList } = this.props.todo;
     return (
       <section className="main">
         <ul className="todo-list">
-          <li className="completed">
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label>sasasssasasssasasssasass</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
-          <li className="">
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label>sasass</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
+          {todoList.map((todo: Todo) => {
+            return (
+              <li className={todo["done"] ? "completed" : ""}>
+                <div className="view">
+                  <input
+                    className="toggle"
+                    type="checkbox"
+                    onClick={() => {
+                      this.handleToggle(todo["id"]);
+                    }}
+                  />
+                  <label>{todo["name"]}</label>
+                  <button
+                    className="destroy"
+                    onClick={() => {
+                      this.handleDelete(todo["id"]);
+                    }}
+                  ></button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </section>
     );
   }
 }
 
-export default Content;
+const mapStateToProps = (state: State) => ({
+  todo: state.todoList
+});
+
+const mapDispatchToProps = {
+  onDeleteTodo: deleteTodo,
+  onToggleTodo: toggleTodo
+};
+
+export default connect<any, any>(
+  mapStateToProps,
+  mapDispatchToProps
+)(Content);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,63 @@
 import * as React from "react";
+import { connect } from "react-redux";
+import { State } from "../store/reducer";
+import { addTodo } from "../store/action";
 
-class Header extends React.Component<{}, {}> {
+interface Props {
+  onAddTodo?: (value: string) => any;
+}
+interface States {
+  value: string;
+}
+
+class Header extends React.Component<Props, States> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
+  handleChange(value: string) {
+    this.setState({ value });
+  }
+
+  handleOnEnter(e: any) {
+    if (e.key === "Enter") {
+      this.props.onAddTodo(this.state.value);
+      this.setState({ value: "" });
+    }
+  }
+
   render() {
+    const { value } = this.state;
     return (
       <header className="header">
         <h1>todoList</h1>
-        <input className="new-todo" placeholder="What needs to be done?" />
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          value={value}
+          onChange={e => this.handleChange(e.target.value)}
+          onKeyDown={e => {
+            this.handleOnEnter(e);
+          }}
+        />
         <label className="toggle-btn"></label>
       </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state: State) => ({
+  todo: state.todoList
+});
+
+const mapDispatchToProps = {
+  onAddTodo: addTodo
+};
+
+export default connect<any, any>(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
