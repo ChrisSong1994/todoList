f5153157630d851e8b0198955f414c3a6970e543
jun song <songjun849@gmail.com>
2020-02-21 16:21:58 +0800

    fix：增加清除dist 文件夹 script

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,12 @@
-import logger from "redux-logger";
-import { createStore, applyMiddleware } from "redux";
-import { reducer } from "./reducer";
-import * as fromTodoList from "./reducer/todoList";
+import logger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import { reducer } from './reducer'
+import * as fromTodoList from './reducer/todoList'
 
 const store = createStore(
   reducer,
   { todoList: fromTodoList.initialState },
   applyMiddleware(logger)
-);
+)
 
-export default store;
+export default store
