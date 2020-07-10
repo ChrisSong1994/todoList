b6744222a537b49edc8ea29f19073f07a3589780
Chris Song <songjun@broada.com>
2020-07-10 11:35:21 +0800

    update:添加调试工具

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,14 @@ import logger from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux'
 import { reducer } from './reducer'
 import * as fromTodoList from './reducer/todoList'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const preloadState= { todoList: fromTodoList.initialState }
 
 const store = createStore(
   reducer,
-  { todoList: fromTodoList.initialState },
-  applyMiddleware(logger)
+  preloadState,
+  composeWithDevTools( applyMiddleware(logger),)
 )
 
 export default store
