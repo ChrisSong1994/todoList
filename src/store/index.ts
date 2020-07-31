8d41da5191aa4d027514b26e9c78b24ec7386720
1352411860@qq.com <songjun@broada.com>
2020-07-31 14:27:06 +0800

    fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
-import logger from 'redux-logger'
-import { createStore, applyMiddleware } from 'redux'
-import { reducer } from './reducer'
-import * as fromTodoList from './reducer/todoList'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { reducer } from "./reducer";
+import * as fromTodoList from "./reducer/todoList";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const preloadState= { todoList: fromTodoList.initialState }
+const preloadState = { todoList: fromTodoList.initialState };
 
-
-const store = createStore(reducer, preloadState, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(
+  reducer,
+  preloadState,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
